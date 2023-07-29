7edec2a21f68b6c99d6cdf263130068e6c3e3bdc
Robert Laye <2000rob@live.co.uk>
2023-07-29 11:24:16 +0100

    Inserted products into homepage

---
+++ front/js/script.js
@@ -0,0 +1,50 @@
+// Variables
+let cardSection = document.getElementById('items');
+let cardImage = document.getElementsByClassName('productImage')
+let cardHeading = document.getElementsByClassName('productName')
+let cardParagraph = document.getElementsByClassName('productDescription')
+let card = document.getElementsByClassName('productCard')
+
+// Function that creates a new card when called
+let createNewCard = () => {
+    let newCard = document.createElement('a');
+    let newArticle = document.createElement('article');
+    let newImage = document.createElement('img');
+    let newHeading = document.createElement('h3');
+    let newParagraph = document.createElement('p');
+    newArticle.appendChild(newImage);
+    newArticle.appendChild(newHeading);
+    newArticle.appendChild(newParagraph);
+    newCard.appendChild(newArticle);
+    cardSection.appendChild(newCard);
+    newImage.classList.add('productImage')
+    newHeading.classList.add('productName')
+    newParagraph.classList.add('productDescription')
+    newCard.classList.add('productCard')
+}
+
+// Creates a new XMLHttpRequest object
+let apiRequest = new XMLHttpRequest();
+
+// Determines the type of request and the URL the request will be sent to.
+apiRequest.open('GET', 'http://localhost:3000/api/products')
+
+// Sends the request
+apiRequest.send();
+
+// Function that stores the API response and then uses it to create and populate new cards.
+apiRequest.onreadystatechange = () => {
+    if (apiRequest.readyState === 4) {
+        const response = JSON.parse(apiRequest.response);
+        for (let i = 0; i < response.length; i++) {
+            createNewCard();
+            card[i].setAttribute('href', './product.html?id=' + response[i]._id);
+            cardImage[i].setAttribute('src', response[i].imageUrl);
+            cardImage[i].setAttribute('alt', response[i].altTxt);
+            cardHeading[i].textContent = response[i].name;
+            cardParagraph[i].textContent = response[i].description;
+        }
+    }
+}
+
+
