1ec65e7a78c1d656ce7383a94fe1f943f57c3095
Robert Laye <2000rob@live.co.uk>
2023-09-18 09:39:46 +0100

    Added fetch array and began working on remove button

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,20 +9,34 @@ let totalPrice = document.getElementById('totalPrice');
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 console.log(cart);
 
+
+let fetchArray = [];
+
+cart.forEach((product) => {
+    fetchArray.push(fetch('http://localhost:3000/api/products/' + product.id).then(response => response.json()))
+});
+
+Promise.all(fetchArray).then(results => {
+    results.forEach((product, i) => {
+        console.log(product);
+        addProduct(product, i);
+     })
+});
+
 // Iterates through each product in the local storage and creates a new entry for each product on the cart page.
-cart.forEach((product, i) => {
+function addProduct(product, i) {
     // Product in cart
     let cartItem = document.createElement('article');
     cartItem.setAttribute('class', 'cart__item');
-    cartItem.setAttribute('data-id', cart[i].id);
+    cartItem.setAttribute('data-id', cart[i].id); 
     cartItem.setAttribute('data-color', cart[i].color);
     // Product image
     let cartItemImageDiv = document.createElement('div');
     cartItemImageDiv.setAttribute('class', 'cart__item__img');
     cartItem.appendChild(cartItemImageDiv);
     let cartItemImage = document.createElement('img');
-    cartItemImage.setAttribute('src', 'something.imageUrl') //placeholder
-    cartItemImage.setAttribute('alt', 'something.altTxt')//placeholder
+    cartItemImage.setAttribute('src', product.imageUrl)
+    cartItemImage.setAttribute('alt', product.altTxt)
     cartItemImageDiv.append(cartItemImage);
     // Product information
     let cartItemContent = document.createElement('div');
@@ -32,11 +46,11 @@ cart.forEach((product, i) => {
     cartItemDescription.setAttribute('class', 'cart__item__content__description');
     cartItemContent.append(cartItemDescription);
     let productName = document.createElement('h2');
-    productName.textContent = 'SOMETHING.VALUE'; //placeholder
+    productName.textContent = product.name;
     let productColor = document.createElement('p');
-    productColor.textContent = cart[i].color; //placeholder cart[i].color(.value?)
+    productColor.textContent = cart[i].color;
     let productPrice = document.createElement('p');
-    productPrice.textContent = 'SOMETHING.VALUE' //placeholder;
+    productPrice.textContent = '€' + product.price;
     cartItemDescription.append(productName, productColor, productPrice);
     // Product settings
     let cartItemSettings = document.createElement('div');
@@ -46,14 +60,14 @@ cart.forEach((product, i) => {
     cartItemSettingsQuantity.setAttribute('class', 'cart__item__content__settings__quantity');
     cartItemSettings.append(cartItemSettingsQuantity);
     let cartItemSettingsQuantityText = document.createElement('p')
-    cartItemSettingsQuantityText.textContent = 'Quantity :' // placeholder
+    cartItemSettingsQuantityText.textContent = 'Quantity :';
     let cartItemSettingsInput = document.createElement('input');
     cartItemSettingsInput.setAttribute('type', 'number');
     cartItemSettingsInput.setAttribute('class', 'itemQuantity');
     cartItemSettingsInput.setAttribute('name', 'itemQuantity');
     cartItemSettingsInput.setAttribute('min', '1');
     cartItemSettingsInput.setAttribute('max', '100');
-    cartItemSettingsInput.setAttribute('value', cart[i].quantity) //placeholder
+    cartItemSettingsInput.setAttribute('value', cart[i].quantity) 
     cartItemSettingsQuantity.append(cartItemSettingsQuantityText, cartItemSettingsInput);
     let cartItemSettingsDelete = document.createElement('div');
     let cartItemSettingsDeleteText = document.createElement('p')
@@ -62,14 +76,34 @@ cart.forEach((product, i) => {
     cartItemSettingsDelete.append(cartItemSettingsDeleteText)
     cartItemSettings.append(cartItemSettingsDelete);
     cartSection.append(cartItem);
-});
+    totalPrice.textContent = product.price * cart[i].quantity;
+    totalQuantity.textContent = cart[i].quantity + cart[i].quantity;
+
+    cartItemSettingsDelete.addEventListener('click', () => {
+        let closestCartItem = cartItemSettingsDelete.closest('.cart__item');
+        closestCartItem.getAttribute('data-id');
+        closestCartItem.getAttribute('data-color');
+        cartItem.remove(closestCartItem);
+        // store cartItem.remove(cartItemSettingsDelete.closest('.cart__item')); into a variable. DONE.
+        // get the data attributes (color, id). DONE.
+        // go to cart from localstorage, look for product with same id and color, then remove it.
+        // store the cart back into the localstorage.
+    })
+
+}
+
 
 /**
  * For this page I need to:
+ * 
  * 1. Get the cart from the localStorage. e.g let cart = JSON.parse(localStorage.getItem('cart')) || []; DONE
+ * 
  * 2. Use a for loop (or .forEach method) to iterate through each product in the cart. DONE
+ * 
  * 2a. use multiple fetch to get the price for each product id (Promise.all([F1, F2, F3,F3]))
+ * 
  * 2b. Get each product property and assign it to an id from the HTML to make it show up on the cart page. (similar to homepage).
+ * 
  * 3. Iterate through the values of each product in the cart (multipied by their quantity) and assign it to the total price.
  * Also get quantity and assign to articles(total quantity).
  * 
@@ -80,15 +114,4 @@ cart.forEach((product, i) => {
  * for both update and delete, on the event object get the closest element (article), 
  * from the article get the data-id and data-color, with the id and color look at the storage (cart), do update or delete on the item from the card
  * re-compute the total items and total price.
- */
-/*
-let fetchArray = [];
-cart.forEach((product) => {
-    fetchArray.push(fetch('www.google.com/productId'));
-    console.log(product.id);
-    console.log(product.color);
-    console.log(product.quantity);
-});
-
-const allData = Promise.all(fetchArray);//updated products info
-*/
+ **/
